8c83cb6c82ad1ba313a1d4241bd57ac2455e9b6c
humberwg <116973554+humberwg@users.noreply.github.com>
2023-03-02 23:54:14 +0100

    Update StartClient.java

--- CalculatorClient/src/StartClient.java
+++ CalculatorClient/src/StartClient.java
@@ -36,11 +36,10 @@ public class StartClient {
 
             while(true) {
                 // asking for input and read it
-                System.out.println("------------------------------------------");
-                System.out.println("Enter the parameters in this format [operator][sp][operand1][sp][operand2]."
-                        + "\nFor example: + 1 2");
+                System.out.println("\n------------------------------------------\n");
+                System.out.println("For help with the operands aviable type 'Operands_av'\n");
                 Scanner c=new Scanner(System.in);
-		String input = c.nextLine();
+		        String input = c.nextLine();
                 
                 // if the command is exit, request the server to shutdown
                 if (input.toLowerCase().equals("exit")) {
@@ -50,13 +49,29 @@ public class StartClient {
                 
                 // test the input
                 String[] inputParams = input.split(" ");
-                if (inputParams.length != 3) {
-                    System.out.println("Client Exception: Wrong number of parameters. Try again...");
+                if (inputParams.length > 3) {
+
+                    System.out.println("Client Exception 1: Wrong number of parameters . Try again...");
+                    
                     continue;
                 }
-                int operatorCode;
-                int operand1;
-                int operand2;
+                    
+
+                if(inputParams[0].equals("Operands_av")){
+                    System.out.println("\nList of operands: add + | substract - |  multiply * | divide / | square root -> sqrt |"
+                        +"\nIn case of square root only uses the first operator [operator][sp][operand1]-> sqrt 2"); 
+                        continue;                   
+                }
+                else if(inputParams[0].equals("sqrt")){
+                    if(inputParams.length != 2){
+                        System.out.println("Client Exception 2: Wrong number of parameters for sqrt. Try again...");
+                        continue;
+                    }
+                }
+                //initialize in case theres no input so code keeps running----------------------
+                int operatorCode=1;
+                int operand1=1;
+                int operand2=1;
                 
                 // set calculation type
                 if (inputParams[0].equals("+")) {
@@ -70,41 +85,47 @@ public class StartClient {
                 }
                 else if (inputParams[0].equals("/")) {
                     operatorCode = 4;
-                }
-                else {
+                }else if (inputParams[0].equals("sqrt")) {
+                    operatorCode = 5;
+                }else if (inputParams[0].equals("Operands_av")){
+                    operatorCode = 6;
+                }else {
                     System.out.println("Client Exception: Un-recognized operation code. Try again...");
                     continue;
                 }
                 
                 // test input operands are integers
                 try {
-                    operand1 = Integer.parseInt(inputParams[1]);
-                    operand2 = Integer.parseInt(inputParams[2]);
+                    //check for not out of range error---------------------------------
+                    if(operatorCode < 5){
+                        operand1 = Integer.parseInt(inputParams[1]);
+                        operand2 = Integer.parseInt(inputParams[2]);
+                    }else if(operatorCode == 5){
+                        operand1 = Integer.parseInt(inputParams[1]);
+                    }
+                    
                 }
                 catch (NumberFormatException e) {
-                    System.out.println("Client Exception: Wrong number format. Try again...");
+                    System.out.println("Client Exception 3: Wrong number format. Try again...");
                     continue;
                 }
                 
                 // check if it is divided by zero
                 if (operatorCode == 4 && operand2 == 0) {
-                    System.out.println("Client Exception: Can't be divided by zero. Try again...");
+                    System.out.println("Client Exception 4: Can't be divided by zero. Try again...");
                     continue;
                 }
                 
-                // do the calculation and return result
-		int result = calcObj.calculate(operatorCode, operand1, operand2);
-                String resultDisplay = "";
+                // do the calculation and return result---------------------------------------------------
+		        float result = calcObj.calculate(operatorCode, operand1, operand2);
                 if (result == Integer.MAX_VALUE) {
-                    resultDisplay = "There might be an Integer Overflow. Please try again...";
+                    System.out.println ("There might be an Integer Overflow. Please try again...");
                 }
                 else if (result == Integer.MIN_VALUE) {
-                    resultDisplay = "There might be an Integer Underflow. Please try again...";
+                    System.out.println ("There might be an Integer Underflow. Please try again...");
                 }
-                else {
-                    resultDisplay = String.valueOf(result);
-                }
-		System.out.println("The result is: " + resultDisplay);
+                
+		        System.out.println("The result is: " + result);
             }
         }
         catch (Exception e) {
